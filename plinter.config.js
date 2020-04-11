846506079ae8b88ef74ed5f5e0f9834c97a1b3b1
Joaquim <joaquimmy@yahoo.com>
2020-04-11 20:26:09 -0300

    chore: update plinter config

--- plinter.config.js
+++ plinter.config.js
@@ -2,6 +2,5 @@ module.exports = {
   rules: {
     'top-level-items': ['error', { items: ['CHANGELOG.md', 'README.md', 'LICENSE', 'src', '.editorconfig'] }],
   },
-  ignore: ['*.md'],
   pattern: ['**'],
 };
